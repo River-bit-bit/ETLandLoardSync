50765cc4ebfe68e34df248a964f36919a7d29b1f
xiongzhen <xiongzhen@bilibili.com>
2021-01-23 15:19:43 +0800

    [功能] 上传工程文件

---
+++ Landlords_Client01/Unity/Assets/Model/Test/TestRoom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+namespace ETModel
+{
+    public sealed class TestRoom : Entity
+    {
+        public CancellationTokenSource waitCts;
+    }
+}
---
+++ Landlords_Client01/Unity/Assets/Model/Test/WaitingTimeBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+
+namespace ETModel
+{
+    [TimeBehavior(Typebehavior.Waiting)]
+    public class WaitingTimeBehavior : ITimeBehavior
+    {
+        public TestRoom room;
+        public long waitTime;
+
+        public void Behavior(Entity parent, long time)
+        {
+            room = parent as TestRoom;
+            waitTime = time;
+            Waiting().Coroutine();
+        }
+
+        
+        public async ETVoid Waiting(){
+            TimerComponent timer = Game.Scene.GetComponent<TimerComponent>();
+            room.waitCts = new CancellationTokenSource();
+
+            await timer.WaitAsync(waitTime,room.waitCts.Token);
+            Log.Info($"{room.GetType().ToString()}-执行完waiting");
+            room.waitCts.Dispose();
+            room.waitCts = null;
+        }
+    }
+}
